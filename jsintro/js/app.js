fee0c4cac5a292554cd7177ac659bbcaa929b7ba
jaynelee04 <63226352+jaynelee04@users.noreply.github.com>
2020-05-11 18:58:30 -0700

    Update app.js

--- jsintro/js/app.js
+++ jsintro/js/app.js
@@ -19,6 +19,8 @@ console.log('-------');
 // Last movie name
 console.log('2. Last movie name:');
 
+console.log(MOVIES[MOVIES.length - 1].title);
+
 // Answer: Toy Story
 
 console.log('-------');
@@ -29,6 +31,8 @@ console.log('-------');
 // Count of movies
 console.log('3. Count of movies:');
 
+console.log(MOVIES.length);
+
 // Answer: 6
 
 console.log('-------');
@@ -38,7 +42,7 @@ console.log('-------');
 
 // Add a movie to the list
 console.log('4. Add movie to list:');
-const newMovie = {
+const funMovie = {
     youtubeId: "eZbzbC9285I",
     title: "The Incredibles",
     released: 2004,
@@ -46,7 +50,7 @@ const newMovie = {
     runningTime: 115,
     posterUrl: "https://i.imgur.com/YiMwigk.jpg"
 };
-
+MOVIES.push(funMovie);
 console.log(MOVIES.length);
 // Answer: 7
 
@@ -58,6 +62,16 @@ console.log('-------');
 
 // List all movie titles
 console.log('5. List all movie titles:');
+
+// for (var i = 0; i < MOVIES.length; i++) {
+//    console.log(MOVIES[i].title);
+// }
+
+MOVIES.forEach (
+    function (item) {
+        console.log(item.title);
+    }
+);
 
 // Answer:
 // The Royal Tenenbaums
@@ -78,6 +92,17 @@ console.log('-------');
 // https://www.youtube.com/watch?v={youtubeId}
 console.log('6. Movie trailers on YouTube:');
 
+MOVIES.forEach (
+    function (item) {
+        console.log(
+            "https://www.youtube.com/watch?v=" + item.youtubeId
+        );
+        console.log(item.youtubeId);
+    }
+);
+
+var number = `111`; 
+console.log(number);
 // Answer:
 // https://www.youtube.com/watch?v=8Eg6yIwP2vs
 // https://www.youtube.com/watch?v=lgo3Hb5vWLE
@@ -96,6 +121,27 @@ console.log('-------');
 // How many movies are R rated?
 console.log('7. "R" rated titles:');
 
+//Keep track of the R movies
+var count = 0;
+
+// Loop through each movie
+MOVIES.forEach(
+    function (movie) {
+        // Check if the movie is R rated
+        if (movie.rated == "R") {
+            // If R rated, add to the counter
+            count = count + 1;
+        }
+
+    }
+);
+
+
+
+//Output total R rated movies
+
+console.log(count);
+
 // Answer: 5
 
 console.log('-------');
@@ -107,6 +153,21 @@ console.log('-------');
 
 // Total time of all movies
 console.log('8. Total time of all movies:');
+
+// Keep track of the total time
+var totalTime = 0
+
+// Loop through each movie
+
+MOVIES.forEach (
+    function (movie) {
+        //Add runningtime of each movie to the total time 
+        totalTime = totalTime + movie.runningTime;
+        }
+);
+
+// Output the total time
+console.log(totalTime)
 
 // Answer: 751
 
@@ -120,6 +181,16 @@ console.log('-------');
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
 console.log('9. Oldest to newest:');
 
+MOVIES.sort(
+    function(first, second) {
+        return first.released - second.released;
+    }
+    );
+MOVIES.forEach (
+    function (movie) {
+        console.log(movie.title)
+    }
+);
 // Answer: 
 // 8½
 // The Graduate
@@ -141,8 +212,13 @@ console.log('-------');
 // "The Royal Tenenbaums", released in 2001, rated "R" and runs 92 minutes
 console.log('10. Description of all movies:');
 
+function formattedName (movie) {
+    var format = '"' + movie.title + '", released in ' + movie.released + ', rated "' + movie.rated + '" and runs ' + movie.runningTime + ' minutes';
+    console.log(format);
+}
 
 MOVIES.forEach(function (movie) {
+
     formattedName(movie);
 });
 // Answer: 
